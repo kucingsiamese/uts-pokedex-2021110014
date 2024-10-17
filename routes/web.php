2f5de9fdb9ad07a7d755d92f42d7ecb02ee089fc
Navanya Anggraeni <isabellaa3095@gmail.com>
2024-10-17 21:39:09 +0700

    after auth

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@ use App\Http\Controllers\PokemonController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('pokemon', PokemonController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
