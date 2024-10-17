1fd842e12a5e3bf76ff82c8c3fba7e0b91fcfc64
Navanya Anggraeni <isabellaa3095@gmail.com>
2024-10-17 19:28:00 +0700

    isi create

---
+++ resources/views/pokemon/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('pokemon.store') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
+        </div>
+
+        <div class="mb-3">
+            <label for="species" class="form-label">Species</label>
+            <input type="text" class="form-control" id="species" name="species" value="{{ old('species') }}">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+@endsection
