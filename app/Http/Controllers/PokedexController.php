64065d6310aba2017330d1a8fc7c9520b8bd2c80
Navanya Anggraeni <isabellaa3095@gmail.com>
2024-10-17 21:44:13 +0700

    pokedex.controller

---
+++ app/Http/Controllers/PokedexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pokemon;
+use Illuminate\Http\Request;
+
+class PokedexController extends Controller
+{
+    public function __invoke()
+{
+    $pokemons = Pokemon::paginate(9);
+    return view('pokedex', compact('pokemons'));
+}
+
+}
