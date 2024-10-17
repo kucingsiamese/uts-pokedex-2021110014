4fee69288be7853d89660601fe58ec9608e9361f
Navanya Anggraeni <isabellaa3095@gmail.com>
2024-10-17 19:04:10 +0700

    isi pokemoncontroller

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Pokemon;
 use Illuminate\Http\Request;
 
 class PokemonController extends Controller
@@ -9,9 +9,15 @@ class PokemonController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function __construct()
     {
-        //
+        $this->middleware('auth')->except(['show']);
+    }
+
+     public function index()
+    {
+        $pokemons = Pokemon::paginate(20);
+        return view('pokemon.index', compact('pokemons'));
     }
 
     /**
@@ -19,7 +25,7 @@ class PokemonController extends Controller
      */
     public function create()
     {
-        //
+        return view('pokemon.create');
     }
 
     /**
@@ -27,15 +33,33 @@ class PokemonController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'species' => 'required|string|max:100',
+            'primary_type' => 'required|string|in:Grass,Fire,Water,Bug,Normal,Poison,Electric,Ground,Fairy,Fighting,Psychic,Rock,Ghost,Ice,Dragon,Dark,Steel,Flying',
+            'weight' => 'numeric|max:99999999|min:0',
+            'height' => 'numeric|max:99999999|min:0',
+            'hp' => 'integer|min:0|max:9999',
+            'attack' => 'integer|min:0|max:9999',
+            'defense' => 'integer|min:0|max:9999',
+            'is_legendary' => 'boolean',
+            'photo' => 'nullable|image|mimes:jpeg,jpg,png,gif,svg|max:2048',
+        ]);
+        if ($request->hasFile('photo')) {
+            $path = $request->file('photo')->store('public/photos');
+            $validated['photo'] = basename($path);
+        }
+        Pokemon::create($validated);
 
+        return redirect()->route('pokemon.index')->with('success', 'Pokemon created successfully');
+        }
     /**
      * Display the specified resource.
      */
     public function show(string $id)
     {
-        //
+        $pokemon = Pokemon::findOrFail($id);
+        return view('pokemon.show', compact('pokemon'));
     }
 
     /**
@@ -43,7 +67,8 @@ class PokemonController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $pokemon = Pokemon::findOrFail($id);
+        return view('pokemon.edit', compact('pokemon'));
     }
 
     /**
@@ -51,7 +76,28 @@ class PokemonController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'species' => 'required|string|max:100',
+            'primary_type' => 'required|string|in:Grass,Fire,Water,Bug,Normal,Poison,Electric,Ground,Fairy,Fighting,Psychic,Rock,Ghost,Ice,Dragon,Dark,Steel,Flying',
+            'weight' => 'numeric|max:99999999|min:0',
+            'height' => 'numeric|max:99999999|min:0',
+            'hp' => 'integer|min:0|max:9999',
+            'attack' => 'integer|min:0|max:9999',
+            'defense' => 'integer|min:0|max:9999',
+            'is_legendary' => 'boolean',
+            'photo' => 'nullable|image|mimes:jpeg,jpg,png,gif,svg|max:2048',
+        ]);
+
+        $pokemon = Pokemon::findOrFail($id);
+
+        if ($request->hasFile('photo')) {
+            $path = $request->file('photo')->store('public/photos');
+            $validated['photo'] = basename($path);
+        }
+
+        $pokemon->update($validated);
+        return redirect()->route('pokemon.index')->with('success', 'Pokemon updated successfully');
     }
 
     /**
@@ -59,6 +105,8 @@ class PokemonController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $pokemon = Pokemon::findOrFail($id);
+        $pokemon->delete();
+        return redirect()->route('pokemon.index')->with('success', 'Pokemon deleted successfully');
     }
 }
